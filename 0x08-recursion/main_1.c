818006a6493b52f2d7b4455062a3e31c3f66959d
Vincent Karoubi <vincent.karoubi@gmail.com>
2022-02-21 11:23:43 +0100

    Add recursion

---
+++ 0x08-recursion/main_1.c
@@ -0,0 +1,12 @@
+void _puts_recursion(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Talk is cheap. Show me the code");
+	return (0);
+}
